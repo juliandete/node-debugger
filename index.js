323873091a4a78a859f49d42283a9d83edcb8063
juliandete <julian@determeyser.info>
2024-10-29 15:55:40 +0100

    asd

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // index.js
-var items = [1, "2", 3, "four", 5, null, undefined, 6]
-const results = []
+var items = [1, "2", 3, "four", 5, null, undefined, 6];
+const results = [];
 
-items.foreach((item) => {
-    if(typeof item === 'number')
-        if(item % 2 === 0) results.push(item* 2)
-
-    else if(item === null)
-    console.log("Found a null item")
-
-    else 
-    {
-        result.push(parseInt(item))
+items.forEach((item) => {  // foreach zu forEach ändern
+    if (typeof item === 'number') {
+        if (item % 2 === 0) {
+            results.push(item * 2);
+        }
+    } else if (item === null) {
+        console.log("Found a null item");
+    } else {
+        // 'result' zu 'results' korrigieren
+        results.push(parseInt(item));
     }
 });
 
-console.log("Original items:", items)
-console.log("Filtered and transformed items:", results)
+console.log("Original items:", items);
+console.log("Filtered and transformed items:", results);
